c9651bc4f04ed9ad02b6ca36dc1246d84661b0a2
sergei.obraztsov <sergei.obraztsov@sshvps.net>
2022-02-08 00:17:39 +0300

    feat: add some function

--- script.js
+++ script.js
@@ -2,16 +2,45 @@
 
 console.log('Птицы готовы к вылету')
 
-const songName = ["solovey", "zarynka", "javoronok" , "slavka" , "drozd", "forest"]
-
 let isSongPlaying = false;
-let currentSong = -1;
+
+let currentSong = "forest";
+
+const playBtn = document.querySelector(".play-btn");
+console.log(playBtn.src)
+
+
+
+const togglePLayBtn = () => {
+  if (isSongPlaying) {
+  isSongPlaying = false;
+  playBtn.src="./img/svg/play.svg"
+  } else {
+    isSongPlaying = true;
+    playBtn.src="./img/svg/pause.svg"
+  }
+}
+
+
+playBtn.addEventListener('click', ()=>{togglePLayBtn()})
+
 const btns = document.querySelectorAll(".navbar-item");
 
-const selectSong = (e) => {
-  let songSrc = `/audio/${e.target.dataset.song}.mp3`;
-  console.log(songSrc)
+const clearActive = () => {
+  btns.forEach(item => {
+    item.classList.remove("active")
+  })
 }
+
+const selectSong = (e) => {
+  let pickedSong = e.target.dataset.song;
+  let songSrc = `/audio/${pickedSong}.mp3`;
+  clearActive();
+  console.log(e.target)
+  if (pickedSong===currentSong) pauseSong();
+}
+
+
 
 btns.forEach(item => {
   item.addEventListener('click', (e) => {
