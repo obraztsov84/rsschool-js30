331112b90f2cc7eb49811a20112296b2d22d5d3f
sergei.obraztsov <sergei.obraztsov@sshvps.net>
2022-02-07 03:21:35 +0300

    feat: add js script file without actual script

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 'use strict'
 
-console.log('all system load')
+console.log('Птицы готовы к вылету')
+
+const songName = ["solovey", "zarynka", "javoronok" , "slavka" , "drozd", "forest"]
+
+let isSongPlaying = false;
+let currentSong = -1;
+const btns = document.querySelectorAll(".navbar-item");
+
+const selectSong = (e) => {
+  let songSrc = `/audio/${e.target.dataset.song}.mp3`;
+  console.log(songSrc)
+}
+
+btns.forEach(item => {
+  item.addEventListener('click', (e) => {
+    selectSong(e);
+  });
+})
